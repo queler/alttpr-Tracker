336eb2b05836617a9c1844b5048a0bae7be88ebb
queler <queler@gmail.com>
2019-06-25 21:09:09 -0400

    scrap work for parsing emos logic

---
+++ mapper.js
@@ -0,0 +1,27 @@
+'use strict' ;
+var fs=require('fs');
+var j=JSON.parse(fs.readFileSync('owinv.json'));
+var reg=JSON.parse(fs.readFileSync('owreg.json'));
+function traverse(obj,func,parents, desc){
+//func(obj,desc)
+   desc=(desc||'root' );
+   parents=(parents||[]);
+   if (Array.isArray(obj)) {
+      
+      obj.forEach((a)=>
+			      traverse(a,func,parents,desc )
+			   );
+	  }
+   else { 
+      func(obj,desc);
+      var newP=parents.concat(obj);
+      traverse(obj.children,
+         func,
+         newP,
+         "child") ;
+      traverse(obj.sections,
+      func,
+      newP,
+      'section') 
+   }
+}
